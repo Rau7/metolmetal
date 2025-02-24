bdee4e9791271a8f0834ed5e1b9788b32197ba66
Alp Toker <tokeralp@gmail.com>
2025-02-24 11:07:55 +0300

    NewsAndDetail

---
+++ app/haberler/page.jsx
@@ -0,0 +1,455 @@
+import React from "react";
+
+const Haberler = () => {
+  return (
+    <section className="py-20">
+      <div className="container px-4 mx-auto">
+        <div className="mb-16 max-w-3xl mx-auto text-center">
+          <h2 className="mt-2 mb-4 text-4xl lg:text-5xl font-bold font-heading">
+            Haberler
+          </h2>
+          <p className="text-lg text-gray-500 leading-loose">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
+            aliquet orci.
+          </p>
+        </div>
+        <div className="flex flex-wrap -mx-4 mb-12">
+          <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+            <div className="p-6 bg-gray-50 rounded-lg">
+              <div className="relative h-40 mb-6">
+                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs px-2 py-1 bg-gray-50 rounded uppercase text-gray-500 font-semibold">
+                  Development
+                </span>
+                <img
+                  className="w-full h-full object-cover rounded-lg"
+                  src="assets/mockup-assets/images/gray-500-horizontal.png"
+                  alt
+                />
+              </div>
+              <span className="inline-block text-xs font-bold text-gray-500">
+                10 jun 2020 19:40
+              </span>
+              <h2 className="mb-2 text-2xl font-bold font-heading">
+                Lorem ipsum dolor sit amet consectutar
+              </h2>
+              <p className="mb-4 text-lg text-gray-500 leading-loose">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
+                aliquet orci.
+              </p>
+              <a
+                className="flex items-center text-lg font-bold text-gray-700 hover:text-gray-800"
+                href="#"
+              >
+                <span>Read More</span>
+                <span>
+                  <svg
+                    className="ml-1 w-5 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+            <div className="p-6 bg-gray-50 rounded-lg">
+              <div className="relative h-40 mb-6">
+                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs px-2 py-1 bg-gray-50 rounded uppercase text-gray-500 font-semibold">
+                  Development
+                </span>
+                <img
+                  className="w-full h-full object-cover rounded-lg"
+                  src="assets/mockup-assets/images/gray-500-horizontal.png"
+                  alt
+                />
+              </div>
+              <span className="inline-block text-xs font-bold text-gray-500">
+                10 jun 2020 19:40
+              </span>
+              <h2 className="mb-2 text-2xl font-bold font-heading">
+                Lorem ipsum dolor sit amet consectutar
+              </h2>
+              <p className="mb-4 text-lg text-gray-500 leading-loose">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
+                aliquet orci.
+              </p>
+              <a
+                className="flex items-center text-lg font-bold text-gray-700 hover:text-gray-800"
+                href="#"
+              >
+                <span>Read More</span>
+                <span>
+                  <svg
+                    className="ml-1 w-5 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+            <div className="p-6 bg-gray-50 rounded-lg">
+              <div className="relative h-40 mb-6">
+                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs px-2 py-1 bg-gray-50 rounded uppercase text-gray-500 font-semibold">
+                  Development
+                </span>
+                <img
+                  className="w-full h-full object-cover rounded-lg"
+                  src="assets/mockup-assets/images/gray-500-horizontal.png"
+                  alt
+                />
+              </div>
+              <span className="inline-block text-xs font-bold text-gray-500">
+                10 jun 2020 19:40
+              </span>
+              <h2 className="mb-2 text-2xl font-bold font-heading">
+                Lorem ipsum dolor sit amet consectutar
+              </h2>
+              <p className="mb-4 text-lg text-gray-500 leading-loose">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
+                aliquet orci.
+              </p>
+              <a
+                className="flex items-center text-lg font-bold text-gray-700 hover:text-gray-800"
+                href="#"
+              >
+                <span>Read More</span>
+                <span>
+                  <svg
+                    className="ml-1 w-5 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+            <div className="p-6 bg-gray-50 rounded-lg">
+              <div className="relative h-40 mb-6">
+                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs px-2 py-1 bg-gray-50 rounded uppercase text-gray-500 font-semibold">
+                  Development
+                </span>
+                <img
+                  className="w-full h-full object-cover rounded-lg"
+                  src="assets/mockup-assets/images/gray-500-horizontal.png"
+                  alt
+                />
+              </div>
+              <span className="inline-block text-xs font-bold text-gray-500">
+                10 jun 2020 19:40
+              </span>
+              <h2 className="mb-2 text-2xl font-bold font-heading">
+                Lorem ipsum dolor sit amet consectutar
+              </h2>
+              <p className="mb-4 text-lg text-gray-500 leading-loose">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
+                aliquet orci.
+              </p>
+              <a
+                className="flex items-center text-lg font-bold text-gray-700 hover:text-gray-800"
+                href="#"
+              >
+                <span>Read More</span>
+                <span>
+                  <svg
+                    className="ml-1 w-5 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+            <div className="p-6 bg-gray-50 rounded-lg">
+              <div className="relative h-40 mb-6">
+                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs px-2 py-1 bg-gray-50 rounded uppercase text-gray-500 font-semibold">
+                  Development
+                </span>
+                <img
+                  className="w-full h-full object-cover rounded-lg"
+                  src="assets/mockup-assets/images/gray-500-horizontal.png"
+                  alt
+                />
+              </div>
+              <span className="inline-block text-xs font-bold text-gray-500">
+                10 jun 2020 19:40
+              </span>
+              <h2 className="mb-2 text-2xl font-bold font-heading">
+                Lorem ipsum dolor sit amet consectutar
+              </h2>
+              <p className="mb-4 text-lg text-gray-500 leading-loose">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
+                aliquet orci.
+              </p>
+              <a
+                className="flex items-center text-lg font-bold text-gray-700 hover:text-gray-800"
+                href="#"
+              >
+                <span>Read More</span>
+                <span>
+                  <svg
+                    className="ml-1 w-5 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+            <div className="p-6 bg-gray-50 rounded-lg">
+              <div className="relative h-40 mb-6">
+                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs px-2 py-1 bg-gray-50 rounded uppercase text-gray-500 font-semibold">
+                  Development
+                </span>
+                <img
+                  className="w-full h-full object-cover rounded-lg"
+                  src="assets/mockup-assets/images/gray-500-horizontal.png"
+                  alt
+                />
+              </div>
+              <span className="inline-block text-xs font-bold text-gray-500">
+                10 jun 2020 19:40
+              </span>
+              <h2 className="mb-2 text-2xl font-bold font-heading">
+                Lorem ipsum dolor sit amet consectutar
+              </h2>
+              <p className="mb-4 text-lg text-gray-500 leading-loose">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
+                aliquet orci.
+              </p>
+              <a
+                className="flex items-center text-lg font-bold text-gray-700 hover:text-gray-800"
+                href="#"
+              >
+                <span>Read More</span>
+                <span>
+                  <svg
+                    className="ml-1 w-5 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div className="hidden w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+            <div className="p-6 bg-gray-50 rounded-lg">
+              <div className="relative h-40 mb-6">
+                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs px-2 py-1 bg-gray-50 rounded uppercase text-gray-500 font-semibold">
+                  Development
+                </span>
+                <img
+                  className="w-full h-full object-cover rounded-lg"
+                  src="assets/mockup-assets/images/gray-500-horizontal.png"
+                  alt
+                />
+              </div>
+              <span className="inline-block text-xs font-bold text-gray-500">
+                10 jun 2020 19:40
+              </span>
+              <h2 className="mb-2 text-2xl font-bold font-heading">
+                Lorem ipsum dolor sit amet consectutar
+              </h2>
+              <p className="mb-4 text-lg text-gray-500 leading-loose">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
+                aliquet orci.
+              </p>
+              <a
+                className="flex items-center text-lg font-bold text-gray-700 hover:text-gray-800"
+                href="#"
+              >
+                <span>Read More</span>
+                <span>
+                  <svg
+                    className="ml-1 w-5 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div className="hidden w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+            <div className="p-6 bg-gray-50 rounded-lg">
+              <div className="relative h-40 mb-6">
+                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs px-2 py-1 bg-gray-50 rounded uppercase text-gray-500 font-semibold">
+                  Development
+                </span>
+                <img
+                  className="w-full h-full object-cover rounded-lg"
+                  src="assets/mockup-assets/images/gray-500-horizontal.png"
+                  alt
+                />
+              </div>
+              <span className="inline-block text-xs font-bold text-gray-500">
+                10 jun 2020 19:40
+              </span>
+              <h2 className="mb-2 text-2xl font-bold font-heading">
+                Lorem ipsum dolor sit amet consectutar
+              </h2>
+              <p className="mb-4 text-lg text-gray-500 leading-loose">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
+                aliquet orci.
+              </p>
+              <a
+                className="flex items-center text-lg font-bold text-gray-700 hover:text-gray-800"
+                href="#"
+              >
+                <span>Read More</span>
+                <span>
+                  <svg
+                    className="ml-1 w-5 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div className="hidden w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+            <div className="p-6 bg-gray-50 rounded-lg">
+              <div className="relative h-40 mb-6">
+                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs px-2 py-1 bg-gray-50 rounded uppercase text-gray-500 font-semibold">
+                  Development
+                </span>
+                <img
+                  className="w-full h-full object-cover rounded-lg"
+                  src="assets/mockup-assets/mockup-assets/images/gray-500-horizontal.png"
+                  alt
+                />
+              </div>
+              <span className="inline-block text-xs font-bold text-gray-500">
+                10 jun 2020 19:40
+              </span>
+              <h2 className="mb-2 text-2xl font-bold font-heading">
+                Lorem ipsum dolor sit amet consectutar
+              </h2>
+              <p className="mb-4 text-lg text-gray-500 leading-loose">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
+                aliquet orci.
+              </p>
+              <a
+                className="flex items-center text-lg font-bold text-gray-700 hover:text-gray-800"
+                href="#"
+              >
+                <span>Read More</span>
+                <span>
+                  <svg
+                    className="ml-1 w-5 h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="text-center">
+          <a
+            className="px-6 py-3 text-gray-50 font-semibold bg-gray-500 hover:bg-gray-600 rounded leading-loose"
+            href="#"
+          >
+            View More Articles
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Haberler;
